2410e8fcee3c8124b46972c77b9b21eb7189c7de
Conny Garpö <dolgan12@gmail.com>
2015-12-24 20:56:15 +0100

    Weather upgrade

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
-$(document).ready(function(){
-  var appID = '6cd3ac112dce0606bce5cbdf400330a0';
+var appID = '6cd3ac112dce0606bce5cbdf400330a0';
 
-  // Get location data from ip-adress information.
+
+function getLocation() {
   $.getJSON('http://ipinfo.io', function(location){
-  console.log(location.city);
-
-
-
+  $('#location').html(location.city + ', ' + location.country);
+  getWeather(location.city, location.country);
     }, "jsonp");
+}
 
+function getWeather(city, country){
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country + '&APPID=' + appID;
+  $.getJSON(url, function(openWeather) {
+    var weather = openWeather.weather[0];
+    $('#description').html(weather.description);
+  }, "jsonp");
+}
+
+
+
+$(document).ready(function(){
+  // Get location data from ip-adress information.
+ getLocation();
 });
